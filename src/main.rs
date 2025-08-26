94024104f14c09aab3efda125e7e661b616463b9
irdali <irdali1996@gmail.com>
2025-08-26 14:20:07 +0900

    test to check the connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 mod agent;
 
-fn main() {
-    println!("Hello, world!");
+use agent::KenAgent;
+use axum::http::response;
+use rig::completion::Chat;
+
+#[tokio::main]
+async fn main() {
+
+    let agent = KenAgent::default();
+    let message = "How are you?";
+    match agent.chat(message, Vec::new()).await {
+        Ok(response) => {
+            println!("{}", response);
+        },
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        }
+
+    };
 }
